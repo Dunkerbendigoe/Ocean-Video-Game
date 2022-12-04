dc06adf616e3c12c915e4b264435adf815395d1c
Ian <setoi@wit.edu>
2022-12-04 15:35:14 -0500

    Refactoring of a few scripts, animations, and tiles

--- Assets/Scripts/Mechanics/CameraScript.cs
+++ Assets/Scripts/Mechanics/CameraScript.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour
+namespace Platformer.Mechanics
 {
-    public float speed = 0.003f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CameraScript : MonoBehaviour
     {
-        Destroy(GetComponent("CinemachineBrain"));
+        public float speed = 0.01f;
 
-        transform.position = transform.position + new Vector3(0, 2.5f, 0);
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            Destroy(GetComponent("CinemachineBrain"));
+            transform.position = transform.position + new Vector3(0, 2.5f, 0);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {   
-        transform.Translate(speed, 0, 0);
+        // Update is called once per frame
+        void Update()
+        {   
+            transform.Translate(speed, 0, 0);
+        }
     }
 }
