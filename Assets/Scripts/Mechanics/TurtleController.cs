edb5f85b053de3361e57aa8514d7ead674fe447c
Ian <setoi@wit.edu>
2022-12-05 05:46:03 -0500

    turtle transformation animation and background tiling issue fixed

---
+++ Assets/Scripts/Mechanics/TurtleController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+using Platformer.Model;
+using Platformer.Core;
+
+namespace Platformer.Mechanics
+{
+    public class TurtleController : KinematicObject
+    {
+
+    }
+}
