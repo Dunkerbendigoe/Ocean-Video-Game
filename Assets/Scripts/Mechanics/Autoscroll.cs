2d0bd077174b98de36146a4e212b2c37fb1ebf0d
Ian <setoi@wit.edu>
2022-12-09 04:35:16 -0500

    major transform refactor and fixes (stable)

--- Assets/Scripts/Mechanics/Autoscroll.cs
+++ Assets/Scripts/Mechanics/Autoscroll.cs
@@ -6,23 +6,20 @@ namespace Platformer.Mechanics
 {
     public class Autoscroll : MonoBehaviour
     {   
-        //GameObject player;
-
         // Start is called before the first frame update
         void Start()
         {
-            //player = GameObject.FindWithTag("Player");
-        }
 
-        void OnTriggerEnter2D() {
-            Camera.main.gameObject.AddComponent<CameraScript>();
-            //player.AddComponent<NoClipperton>();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        void OnTriggerEnter2D() {
+            Camera.main.gameObject.AddComponent<CameraScript>();
         }
     }
 }
